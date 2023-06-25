7959414f190b4a541490cb06ff64457a10239b98
Alec McCue <Alecxander.McCue@commscope.com>
2023-06-25 17:24:57 -0400

    tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'turquoise': '#015D67',
+      'dark-cyan': '#00ACB1',
+      'light-aqua':  "#87E4DB",
+      'tea-green': '#CAF0C1',
+      'greyser': '#D5E4E6',
+      'light-grey': '#EBF2F3',
+      'azure': '#FAFCFC',
+      'white': '#FEFEFE',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
