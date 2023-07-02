5beb7929c14c6aff21ce476929123277236efa43
Alec McCue <Alecxander.McCue@commscope.com>
2023-07-01 21:45:01 -0400

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,21 @@
-import Link from 'next/link'
+"use client";
+
+import { Typography } from "@mui/material";
+import { AboutWrapper, IntroWrapper, OfficeImage } from "./styles";
 
 const About = () => {
   return (
-    <div>
-      <Link href="/">Dashboard</Link>
-    </div>
-  )
-}
+    <AboutWrapper>
+      <IntroWrapper>
+        <Typography style={{ fontSize: "3rem" }}>
+          About Family & Cosmetic Dentistry
+        </Typography>
+        <Typography style={{ fontSize: "1.5rem" }}>
+          Meet our excellent team
+        </Typography>
+      </IntroWrapper>
+    </AboutWrapper>
+  );
+};
 
-export default About
+export default About;
