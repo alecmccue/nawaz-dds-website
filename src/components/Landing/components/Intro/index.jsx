4fdbd88447b1a64d33ed5e04e4671f3a70406fd8
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-07-01 22:04:18 -0400

    landing page main initial finished

--- src/components/Landing/components/Intro/index.jsx
+++ src/components/Landing/components/Intro/index.jsx
@@ -1,8 +1,35 @@
-import React from "react"
-const Intro = () =>{
-    return(
-        <div>
+import React from "react";
+import {
+  IntroImage,
+  IntroTitleText1,
+  IntroTitleText2,
+  CallButton,
+  DirectionsButton,
+  BannerImg,
+} from "../../styles";
+import MapIcon from "@mui/icons-material/Map";
+import PhoneIcon from "@mui/icons-material/Phone";
+const Intro = () => {
+  return (
+    <IntroImage style={{ display: "flex" }}>
+      <div>
+        <IntroTitleText1>Feel Good About</IntroTitleText1>
+        <IntroTitleText2>Your Smile</IntroTitleText2>
+        <div style={{ display: "flex" }}>
+          <CallButton>
+            <PhoneIcon style={{ marginRight: "0.25rem" }} />
+            (703) 430 3838
+          </CallButton>
+          <DirectionsButton>
+            <MapIcon style={{ marginRight: "0.25rem" }} />
+            Get Directions
+          </DirectionsButton>
         </div>
-    )
-}
-export default Intro
+      </div>
+      <div>
+        <BannerImg />
+      </div>
+    </IntroImage>
+  );
+};
+export default Intro;
--- src/components/Landing/styles.jsx
+++ src/components/Landing/styles.jsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import {Typography, Box} from "@mui/material";
 
 export const LandingWrapper = styled.div`
     background-color: #FEFEFE;
@@ -11,4 +12,75 @@ export const LandingWrapper = styled.div`
     @media (max-width: 1024px) {
         flex-direction: column;
     }
+`
+export const IntroImage = styled.div`
+    width: 100%;
+    height: 700px;
+    flex-shrink: 0;
+    background: linear-gradient(0deg, rgba(1, 93, 103, 0.20) 0%, rgba(1, 93, 103, 0.20) 100%), url(/clean-dental-office.jpg), lightgray 50% / cover no-repeat;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`
+export const IntroTitleText1 = styled(Typography)`
+    color: #FEFEFE;
+    font-size: 85px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 95px;
+    text-transform: uppercase;
+    margin-left: 4rem;
+    margin-top: 10rem;
+`
+export const IntroTitleText2 = styled(Typography)`
+    color: #FEFEFE;
+    font-size: 85px;
+    font-style: italic;
+    font-weight: 800;
+    line-height: 95px;
+    text-transform: uppercase;
+    margin-left: 4rem;
+    margin-top: .5rem;
+`
+export const CallButton = styled(Box)`
+    background-color: #00ACB1;
+    border-radius: 1rem;
+    border: 3px solid #00ACB1;
+    color: white;
+    font-size: 1.1rem;
+    font-weight: 500;
+    margin-top: 3rem;
+    padding: 0.5rem 1.5rem 0.5rem 1.2rem; 
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    margin-left: 4rem;
+
+    &:hover {
+        background-color: transparent;
+    }
+`
+
+export const DirectionsButton = styled(Box)`
+    background-color: #00ACB1;
+    border-radius: 1rem;
+    border: 3px solid #00ACB1;
+    color: white;
+    font-size: 1.1rem;
+    font-weight: 500;
+    margin-top: 3rem;
+    padding: 0.5rem 1.5rem 0.5rem 1.5rem; 
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    margin-left: 2rem;
+    &:hover {
+        background-color: transparent;
+    }
+`
+export const BannerImg = styled.div`
+    flex-shrink: 0;
+    width: 550px;
+    height: 500px;
+    background: url(/imgbanner.png), 100% / cover no-repeat;
+    margin-top:6rem;
+    margin-left:2rem;
 `
