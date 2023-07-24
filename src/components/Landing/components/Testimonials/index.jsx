ffc09fcb96fbdfa791e1e770994a93f816aa0d90
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-07-24 01:08:28 -0400

    testimonials layout done

--- src/components/Landing/components/Testimonials/index.jsx
+++ src/components/Landing/components/Testimonials/index.jsx
@@ -1,84 +1,88 @@
 import React from "react";
 import StarIcon from "@mui/icons-material/Star";
 import Image from "next/image";
-import { Border, Col, Inner, Row, Testimonial } from "./styles";
+import { Border, Col, Inner, Review, Row, Testimonial, Name } from "./styles";
 const Testimonials = () => {
   return (
     <Testimonial>
-
       <Inner>
         <h1>Testimonials</h1>
-        <Border/>
+        <Border />
         <Row>
-        <Col>
-          <div className="testimonial">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-              alt=""
-              width={100}
+          <Col>
+            <Review>
+              <Image
+                src="/review_icon.png"
+                width={100}
+                height={100}
+                alt="profile picture"
+                style={{margin:'auto'}}
               />
-            <div className="name">Full Name</div>
-            <div className="stars">
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-            </div>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-            repudiandae, pariatur porro a nulla dolorem animi distinctio vitae
-            sapiente nobis aperiam praesentium assumenda doloremque consequatur?
-            Cupiditate voluptatum perspiciatis deleniti veritatis?
-          </p>
-        </Col>
-        <Col>
-          <div className="testimonial">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-              alt=""
-              width={100}
+              <Name>Full Name</Name>
+              <div className="stars">
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+                repudiandae, pariatur porro a nulla dolorem animi distinctio
+                vitae sapiente nob
+              </p>
+            </Review>
+          </Col>
+          <Col>
+            <Review>
+              <Image
+                src="/review_icon.png"
+                width={100}
+                height={100}
+                alt="profile picture"
+                style={{margin:'auto'}}
               />
-            <div className="name">Full Name</div>
-            <div className="stars">
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-            </div>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-            repudiandae, pariatur porro a nulla dolorem animi distinctio vitae
-            sapiente nobis aperiam praesentium assumenda doloremque consequatur?
-            Cupiditate voluptatum perspiciatis deleniti veritatis?
-          </p>
-        </Col>
-        <Col>
-          <div className="testimonial">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-              alt=""
-              width={100}
+              <Name>Full Name</Name>
+              <div className="stars">
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+                repudiandae, pariatur porro a nulla dolorem animi distinctio
+                vitae sapiente nobis aperiam praesentium assumenda doloremque
+                consequatur? Cupiditate voluptatum perspiciatis deleniti
+                veritatis?
+              </p>
+            </Review>
+          </Col>
+          <Col>
+            <Review>
+              <Image
+                src="/review_icon.png"
+                width={100}
+                height={100}
+                alt="profile picture"
+                style={{margin:'auto'}}
               />
-            <div className="name">Full Name</div>
-            <div className="stars">
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-            </div>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-            repudiandae, pariatur porro a nulla dolorem animi distinctio vitae
-            sapiente nobis aperiam praesentium assumenda doloremque consequatur?
-            Cupiditate voluptatum perspiciatis deleniti veritatis?
-          </p>
-        </Col>
+              <Name>Full Name</Name>
+              <div className="stars">
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+                repudiandae, pariatur porro a nulla dolorem animi distinctio
+                vitae sapiente nobis aperia
+              </p>
+            </Review>
+          </Col>
         </Row>
       </Inner>
     </Testimonial>
--- src/components/Landing/components/Testimonials/styles.jsx
+++ src/components/Landing/components/Testimonials/styles.jsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const Testimonial = styled.div`
     padding:40px 0;
-    background: #FEFEFE;
+    background: #EBF2F3;
     color: #434343;
     text-align:center;
 `
@@ -25,5 +25,22 @@ export const Row = styled.div`
 `
 export const Col = styled.div`
     flex:33.33%;
+    max-width:33.33%;
+    box-sizing: border-box;
+    padding:15px;
+    display:flex;
+    justify-content:center;
 
+`
+export const Name = styled.div`
+    font-size:20px;
+    text-transform:uppercase;
+    margin:20px 0
+
+`
+export const Review = styled.div`
+    background: #FEFEFE;
+    padding:30px;
+    justify-content:center;
+    text-align:center;
 `
