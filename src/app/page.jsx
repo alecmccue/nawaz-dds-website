92326eaa112d16851b71d038a1774cee1a5ad52b
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-08-05 12:46:08 -0400

    animation for landing and products (crude)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
-"use client";
-
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import FAQs from "./home/components/FAQs";
 import InformationCards from "./home/components/InformationCards";
 import Intro from "./home/components/Intro";
@@ -8,14 +8,76 @@ import Main from "./home/components/Main";
 import Testimonials from "./home/components/Testimonials";
 import { HomeWrapper } from "./styles";
 
-const Home = () => (
+const Home = () => {
+  const [infoInView, setInfoInView] = useState(false);
+  const [mainInView, setMainInView] = useState(false);
+  const [testInView, setTestInView] = useState(false);
+  const [faqInView, setFaqInView] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const infoPosition = 300;
+      const mainPosition = 450;
+      const testPosition = 1000;
+      const faqPosition = 1700;
+
+      const scrollY = window.scrollY;
+
+      setInfoInView(scrollY > infoPosition);
+      setMainInView(scrollY > mainPosition);
+      setTestInView(scrollY > testPosition);
+      setFaqInView(scrollY > faqPosition);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
     <HomeWrapper>
+      <motion.div
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <Intro />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: infoInView ? 1 : 0, y: infoInView ? 0 : 50 }}
+        transition={{ duration: 0.5 }}
+      >
         <InformationCards />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: mainInView ? 1 : 0, y: mainInView ? 0 : 50 }}
+        transition={{ duration: 0.5 }}
+      >
         <Main />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: testInView ? 1 : 0, y: testInView ? 0 : 50 }}
+        transition={{ duration: 0.5 }}
+      >
         <Testimonials />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: faqInView ? 1 : 0, y: faqInView ? 0 : 50 }}
+        transition={{ duration: 0.5 }}
+      >
         <FAQs />
+      </motion.div>
     </HomeWrapper>
-)
+  );
+};
 
 export default Home;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,19 +1,68 @@
-"use client";
-
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { ProductsWrapper } from "./styles";
 import DigitalXray from "./components/DigitalXray";
 import IntraoralScanner from "./components/IntraoralScanner";
 import Introduction from "./components/Introduction";
 import ZoomWhitening from "./components/ZoomWhitening";
 
-const Products = () => (
-  <ProductsWrapper>
-    <Introduction />
-    <ZoomWhitening />
-    <IntraoralScanner />
-    <DigitalXray />
-  </ProductsWrapper>
-);
+const Products = () => {
+  const [intraoralInView, setIntraoralInView] = useState(false);
+  const [digitalXrayInView, setDigitalXrayInView] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const intraoralPosition = 500;
+      const digitalXrayPosition = 700;
+
+      const scrollY = window.scrollY;
+
+      setIntraoralInView(scrollY > intraoralPosition);
+      setDigitalXrayInView(scrollY > digitalXrayPosition);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <ProductsWrapper>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0}}
+        transition={{ duration: 0.5 }}
+      >
+        <Introduction />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <ZoomWhitening />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: intraoralInView ? 1 : 0, y: intraoralInView ? 0 : 50 }}
+        transition={{ duration: 0.5 }}
+      >
+        <IntraoralScanner />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: digitalXrayInView ? 1 : 0, y: digitalXrayInView ? 0 : 50 }}
+        transition={{ duration: 0.5 }}
+      >
+        <DigitalXray />
+      </motion.div>
+    </ProductsWrapper>
+  );
+};
 
 export default Products;
