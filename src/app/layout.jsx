cb0fd8dd5002ae733ad31e5686418c34354bd6c2
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-08-12 23:24:10 -0400

    fixed favicon and title and description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,12 @@
 import "./globals.css";
 import Providers from "@/theme/theme-provider";
 
+export const metadata = {
+  title:"Family And Cosmetic Dentistry",
+  content:"Family And Cosmetic Dentistry is your Trusted Local Dentist in Sterling, VA. At our cozy and modern dental practice, we prioritize your oral health and comfort above all else.",
+  keywords: ["Dentist", "Healthcare", "Virginia", "Local"],
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
