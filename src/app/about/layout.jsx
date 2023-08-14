7a1fcaec2deb127dd5c4910740f0fab15fa7718b
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-08-14 19:53:37 -0400

    some metadata changes

--- src/app/about/layout.jsx
+++ src/app/about/layout.jsx
@@ -1,5 +1,6 @@
 export const metadata = {
     title:"About Us",
+    description:"Our mission is to create a transformative impact on your life through exceptional dentistry"
 }
 export default function About({ children }) {
     return <section>{children}</section>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,12 @@ import Providers from "@/theme/theme-provider";
 
 export const metadata = {
   title:"Family And Cosmetic Dentistry",
-  content:"Family And Cosmetic Dentistry is your Trusted Local Dentist in Sterling, VA. At our cozy and modern dental practice, we prioritize your oral health and comfort above all else.",
+  openGraph: {
+    title: "Family And Cosmetic Dentistry",
+    description: "Family And Cosmetic Dentistry is your Trusted Local Dentist in Sterling",
+    url:"https://www.nawazdds.com/",
+  },
+  description:"Family And Cosmetic Dentistry is your Trusted Local Dentist in Sterling, VA. At our cozy and modern dental practice, we prioritize your oral health and comfort above all else.",
   keywords: ["Dentist", "Healthcare", "Virginia", "Local"],
 }
 
