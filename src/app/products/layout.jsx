fb83452bf4ebf61ae99aa4b46247623558289eae
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-08-14 20:02:21 -0400

    added page titles for each page

---
+++ src/app/products/layout.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title:"Products",
+    description:"We have curated a selection of state-of-the-art products to enhance your oral health journey"
+}
+export default function Products({ children }) {
+    return <section>{children}</section>
+}
