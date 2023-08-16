3c16bbafe4c687f835853386765c3ba3d6852580
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-08-15 21:49:53 -0400

    fixed the error regarding navbar and then changed animation for home

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,9 +4,7 @@ import MobileNavbar from "@/components/Navbar/MobileNavbar";
 import { AppBar, Toolbar } from "@mui/material";
 
 const Navbar = () => {
-    const [screenWidth, setScreenWidth] = useState(
-        typeof window !== 'undefined' ? window.innerWidth : 0
-      );
+    const [screenWidth, setScreenWidth] = useState(window ? window.innerWidth : 0);
 
     const updateScreenSize = () => {
         setScreenWidth(window.innerWidth);
