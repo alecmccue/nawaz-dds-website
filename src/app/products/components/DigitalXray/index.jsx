dfb8d141f7059457bd43d08c202ff7cb5079cd2f
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-08-15 23:08:11 -0400

    updated zoom whitening and digital xray descriptions

--- src/app/products/components/DigitalXray/index.jsx
+++ src/app/products/components/DigitalXray/index.jsx
@@ -9,34 +9,32 @@ import {
 import { Typography } from "@mui/material";
 
 const DigitalXray = () => (
-    <ProductWrapperImageRight style={{ marginBottom: "2rem" }}>
-      <TextColumn>
-        <Typography style={{ fontSize: "2.2rem" }} className="poppins">
-          Digital Xray
-        </Typography>
-        <Divider />
-        <Typography style={{ fontSize: "1.2rem" }} className="scope-one">
-          Digital X-ray is an advanced imaging technology that revolutionizes the
-          way dental professionals capture and analyze images of a patient's oral
-          structures. This digital radiography technique replaces traditional
-          film-based X-rays with a digital sensor that captures highly detailed
-          and instantaneous images of the teeth, gums, and surrounding tissues.
-          The process involves placing the sensor inside the patient's mouth,
-          which emits a significantly lower level of radiation compared to
-          conventional X-ray films. The captured digital X-rays are then displayed
-          on a computer screen, allowing dentists to zoom, enhance, and manipulate
-          the images for better diagnosis and treatment planning. With its reduced
-          radiation exposure, rapid image acquisition, and enhanced diagnostic
-          capabilities, dentistry digital X-ray has become an indispensable tool
-          in modern dental practices, enabling more efficient and precise oral
-          healthcare.
-        </Typography>
-      </TextColumn>
+  <ProductWrapperImageRight style={{ marginBottom: "2rem" }}>
+    <TextColumn>
+      <Typography style={{ fontSize: "2.2rem" }} className="poppins">
+        Digital Xray
+      </Typography>
+      <Divider />
+      <Typography style={{ fontSize: "1.2rem" }} className="scope-one">
+        For decades now, unlike any major medical imaging methods such as
+        ultrasound, nuclear medicine etc, all of which are digital, conventional
+        x-ray imaging remains a largely analog technology. Making the transition
+        from analog to digital brings several advantages to x-ray imaging. These
+        would include improvement in contrast and other aspects of image quality
+        by means of image processing: radiological images could be compared more
+        with those obtained from other imaging modalities, electronic
+        distribution of images within hospitals could make remote access and
+        archiving possible, highly qualified personnel could service remote or
+        poorly populated regions from a central facility by means of
+        "teleradiology"; and radiologists could use computers more effectively
+        to help with diagnosis. healthcare.
+      </Typography>
+    </TextColumn>
 
-      <ImageColumn>
-        <DigitalXrayImage src="/digital_xray.jpg" />
-      </ImageColumn>
-    </ProductWrapperImageRight>
+    <ImageColumn>
+      <DigitalXrayImage src="/digital_xray.jpg" />
+    </ImageColumn>
+  </ProductWrapperImageRight>
 );
 
 export default DigitalXray;
--- src/app/products/components/ZoomWhitening/index.jsx
+++ src/app/products/components/ZoomWhitening/index.jsx
@@ -1,37 +1,44 @@
 import React from "react";
-import { Divider, ImageColumn, ProductWrapperImageRight, TextColumn } from "../../styles";
+import {
+  Divider,
+  ImageColumn,
+  ProductWrapperImageRight,
+  TextColumn,
+} from "../../styles";
 import { Typography } from "@mui/material";
 import Image from "next/image";
 import zoomWhitening from "public/zoom_whitening.jpg";
 
 const ZoomWhitening = () => (
-    <ProductWrapperImageRight>
-        <TextColumn>
-            <Typography style={{ fontSize: "2.2rem" }} className="poppins">
-                Zoom Whitening
-            </Typography>
-            <Divider />
-            <Typography style={{ fontSize: "1.2rem" }} className="scope-one">
-                Zoom Whitening is a highly effective and popular teeth whitening
-                procedure that offers remarkable results in a single visit. Using
-                advanced LED technology, the Zoom Whitening treatment involves the
-                application of a powerful yet safe hydrogen peroxide gel on the teeth.
-                The specialized Zoom lamp is then used to activate the gel, breaking
-                down stubborn stains and discolorations deep within the enamel. This
-                process accelerates the whitening process, revealing noticeably brighter
-                and whiter teeth in just about an hour. Moreover, Zoom Whitening is
-                customizable, allowing the dentist to adjust the intensity of the
-                treatment to ensure optimal comfort and minimize sensitivity. With its
-                convenience, speed, and dramatic outcomes, Dentistry Zoom Whitening is
-                an excellent option for individuals seeking a radiant and confident
-                smile.
-            </Typography>
-        </TextColumn>
+  <ProductWrapperImageRight>
+    <TextColumn>
+      <Typography style={{ fontSize: "2.2rem" }} className="poppins">
+        Zoom™ Whitening
+      </Typography>
+      <Divider />
+      <Typography style={{ fontSize: "1.2rem" }} className="scope-one">
+        Zoom!™ is a revolutionary, in-office tooth whitening system that is
+        safe, effective, and very fast! In just over an hour your teeth will be
+        dramatically whiter. Teeth typically become at least six to ten shades
+        whiter. Zoom!™ Tooth Whitening is ideal for anyone looking for immediate
+        results. The convenience of Zoom!™ in comparison to days of wearing
+        trays and gradual whitening makes it the perfect choice for the busy
+        individual. The Zoom!™ Tooth Whitening procedure is simple and painless.
+        It begins with a short preparation to isolate your lips and gums. The
+        Zoom!TM clinician then applies the proprietary Zoom!™ Tooth Whitening
+        Gel, which is activated by a specially designed light. Teeth typically
+        become at least six to ten shades whiter, sometimes more! A five-minute
+        flouride treatment completes the procedure. Call us today to find out
+        more at (703) 430-3838. For more details about our tooth whitening
+        procedure, please visit the{" "}
+        <a href="https://www.zoomnow.com/">Zoom!™ Tooth Whitening website</a>.
+      </Typography>
+    </TextColumn>
 
-        <ImageColumn>
-            <Image src={zoomWhitening} alt="zoom whitening" />
-        </ImageColumn>
-    </ProductWrapperImageRight>
+    <ImageColumn>
+      <Image src={zoomWhitening} alt="zoom whitening" />
+    </ImageColumn>
+  </ProductWrapperImageRight>
 );
 
 export default ZoomWhitening;
