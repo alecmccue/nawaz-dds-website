405f35386cd1ff0a5a66b4bd84bd97e34bf116a6
Alec McCue <alecmccue14@gmail.com>
2023-08-23 13:01:02 -0400

    added snackbar on appointment request

--- src/app/request-appointment/page.jsx
+++ src/app/request-appointment/page.jsx
@@ -5,13 +5,15 @@ import "react-datepicker/dist/react-datepicker.css";
 import DatePickerInput from "@/app/request-appointment/components/DatePickerInput";
 import PhoneNumberInput from "@/app/request-appointment/components/PhoneNumberInput";
 import emailjs from '@emailjs/browser';
-import { Button, TextField } from "@mui/material";
+import { Alert, Button, Snackbar, TextField } from "@mui/material";
 import { Instructions, RequestAppointmentForm, RequestAppointmentWrapper, Title } from "./styles";
 
 const RequestAppointment = () => {
     const form = useRef();
     const [isPhoneNumberError, setIsPhoneNumberError] = useState(false)
     const [isFormValid, setIsFormValid] = useState(false)
+    const [isSnackbarOpen, setIsSnackbarOpen] = useState(false)
+    const [severity, setSeverity] = useState()
 
     const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY
     const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID
@@ -27,65 +29,87 @@ const RequestAppointment = () => {
         setIsFormValid(isValid)
     }
 
-    const sendEmail = (e) => {
+    const sendEmail = async (e) => {
         e.preventDefault();
 
         // Put snackbar for successful or failed email sent
-        emailjs.sendForm(serviceId, templateId, form.current, publicKey)
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            })
+        try {
+            await emailjs.sendForm(serviceId, templateId, form.current, publicKey)
+            setSeverity("success")
+            setIsSnackbarOpen(true)
+        } catch (e) {
+            setSeverity("error")
+            setIsSnackbarOpen(true)
+            console.log(e)
+        }
     }
 
     return (
-        <RequestAppointmentWrapper>
-            <Title className="arvo">
-                Request Appointment
-            </Title>
-            <Instructions className="arvo">
-                Fill out the form to request an appointment at Family & Cosmetic Dental.
-                <br />
-                Note: This form does not confirm an appointment; the front desk will contact you to confirm your
-                specific appointment date and time.
-            </Instructions>
+        <>
+            <RequestAppointmentWrapper>
+                <Title className="arvo">
+                    Request Appointment
+                </Title>
+                <Instructions className="arvo">
+                    Fill out the form to request an appointment at Family & Cosmetic Dental.
+                    <br />
+                    Note: This form does not confirm an appointment; the front desk will contact you to confirm your
+                    specific appointment date and time.
+                </Instructions>
 
-            <RequestAppointmentForm
-                id="appointment-request-form"
-                ref={form}
-                onSubmit={sendEmail}
-                onChange={validateForm}
-            >
-                <TextField
-                    label="Name"
-                    name="name"
-                    variant="standard"
-                    required
-                />
-                <PhoneNumberInput
-                    isPhoneNumberError={isPhoneNumberError}
-                    name="phone_number"
-                    setIsPhoneNumberError={setIsPhoneNumberError}
-                />
-                <DatePickerInput name="appointment_date_time" />
-                <TextField
-                    label="Reason for Appointment"
-                    name="message"
-                    variant="standard"
-                    required
-                    multiline
-                />
-                <Button
-                    style={{ marginTop: "2rem" }}
-                    type="submit"
-                    variant="outlined"
-                    disabled={!isFormValid}
+                <RequestAppointmentForm
+                    id="appointment-request-form"
+                    ref={form}
+                    onSubmit={sendEmail}
+                    onChange={validateForm}
                 >
-                    Send
-                </Button>
-            </RequestAppointmentForm>
-        </RequestAppointmentWrapper>
+                    <TextField
+                        label="Name"
+                        name="name"
+                        variant="standard"
+                        required
+                    />
+                    <PhoneNumberInput
+                        isPhoneNumberError={isPhoneNumberError}
+                        name="phone_number"
+                        setIsPhoneNumberError={setIsPhoneNumberError}
+                    />
+                    <DatePickerInput name="appointment_date_time" />
+                    <TextField
+                        label="Reason for Appointment"
+                        name="message"
+                        variant="standard"
+                        required
+                        multiline
+                    />
+                    <Button
+                        style={{ marginTop: "2rem" }}
+                        type="submit"
+                        variant="outlined"
+                        disabled={!isFormValid}
+                    >
+                        Send
+                    </Button>
+                </RequestAppointmentForm>
+            </RequestAppointmentWrapper>
+            <Snackbar
+                anchorOrigin={{ horizontal: "right", vertical: "top" }}
+                autoHideDuration={6000}
+                onClose={() => setIsSnackbarOpen(false)}
+                open={isSnackbarOpen}
+            >
+                <Alert
+                    onClose={() => setIsSnackbarOpen(false)}
+                    severity={severity}
+                    sx={{ width: '100%' }}
+                >
+                    {severity === "success"
+                        ? "Your request was successfully sent."
+                        : "There was an error with your request. Try again."
+                    }
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
 
