5cc8f996a513d7ea4529c42e3a7a057f9fb278b9
Alec McCue <alecmccue14@gmail.com>
2023-08-26 12:32:14 -0400

    fixed hover buttons and added phoen number to about

--- src/components/Footer/components/Address/index.jsx
+++ src/components/Footer/components/Address/index.jsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import PlaceIcon from "@mui/icons-material/Place";
 import { FooterIconWrapper } from "../../styles";
 import { StackItemText, StackItemWrapper } from "../../../styles";
-import PlaceIcon from "@mui/icons-material/Place";
 
 const Address = ({ handleOpenDirectionsModal }) => {
-  const [isHover, setIsHover] = useState(false);
+    const [isHover, setIsHover] = useState(false);
 
-  return (
-    <StackItemWrapper
-      onClick={handleOpenDirectionsModal}
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
-    >
-      <FooterIconWrapper
-        style={{
-          backgroundColor: isHover ? "transparent" : "#00ACB1",
-          color: isHover ? "#00ACB1" : "white",
-        }}
-      >
-        <PlaceIcon />
-      </FooterIconWrapper>
-      <StackItemText style={{ color: isHover ? "#00ACB1" : "black" }}>
-        46179 Westlake Drive, Suite 230,
-        <br />
-        Sterling, VA 20165
-      </StackItemText>
-    </StackItemWrapper>
-  );
-};
+    return (
+        <StackItemWrapper
+            onClick={handleOpenDirectionsModal}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            style={{ cursor: "pointer" }}
+        >
+            <FooterIconWrapper
+                style={{
+                    backgroundColor: isHover ? "transparent" : "#00ACB1",
+                    color: isHover ? "#00ACB1" : "white",
+                }}
+            >
+                <PlaceIcon />
+            </FooterIconWrapper>
+            <StackItemText style={{ color: isHover ? "#00ACB1" : "black" }}>
+                46179 Westlake Drive, Suite 230,
+                <br />
+                Sterling, VA 20165
+            </StackItemText>
+        </StackItemWrapper>
+    )
+}
 
-export default Address;
+export default Address
--- src/components/styles.jsx
+++ src/components/styles.jsx
@@ -2,28 +2,27 @@ import styled from "@emotion/styled";
 import { Typography } from "@mui/material";
 
 export const IconWrapper = styled.div`
-    background-color: #00ACB1;
-    border-radius: 50%;
-    border: thin solid #00ACB1;
-    padding: 0.4rem;
-    color: white;
+  background-color: #00ACB1;
+  border-radius: 50%;
+  border: thin solid #00ACB1;
+  padding: 0.4rem;
+  color: white;
 
-    &:hover {
-        background-color: transparent;
-        color: #00ACB1;
-    }
+  &:hover {
+    background-color: transparent;
+    color: #00ACB1;
+  }
 `
 
 export const StackItemText = styled(Typography)`
-    display: flex;
-    align-items: center;
-    margin-left: 1rem;
+  display: flex;
+  align-items: center;
+  margin-left: 1rem;
 `
 
 export const StackItemWrapper = styled.div`
-    align-items: center;
-    cursor: pointer;
-    display: flex;
-    flex-direction: row;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
 `
 
