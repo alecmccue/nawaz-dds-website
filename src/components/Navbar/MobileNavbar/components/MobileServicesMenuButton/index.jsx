2ca286296028063fd54b81bc9cc585dbd5143ea5
Alec McCue <alecmccue14@gmail.com>
2023-08-26 13:12:28 -0400

    increase timeout

--- src/components/Navbar/MobileNavbar/components/MobileServicesMenuButton/index.jsx
+++ src/components/Navbar/MobileNavbar/components/MobileServicesMenuButton/index.jsx
@@ -17,7 +17,7 @@ const MobileServicesMenuButton = (props) => {
             if(element) {
                 element.scrollIntoView({ behavior: "smooth", block: "start" });
             }
-        }, 100);
+        }, 200);
     }, [elementId])
 
     return (
