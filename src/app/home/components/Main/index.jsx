72865ffb212091c41962d25e4f42e6433536436c
Alec McCue <alecmccue14@gmail.com>
2023-08-26 17:29:53 -0400

    fixed button links

--- src/app/home/components/Main/index.jsx
+++ src/app/home/components/Main/index.jsx
@@ -3,65 +3,65 @@ import "../../../fonts.css";
 import Link from "next/link";
 import { StarSharp } from "@mui/icons-material";
 import {
-  Description,
-  DescriptionWrapper,
-  DescriptionLists,
-  DescriptionTitle,
-  DescriptionSubtitle,
-  MainButton,
-  MainWrapper,
-  NawazImg,
+    Description,
+    DescriptionWrapper,
+    DescriptionLists,
+    DescriptionTitle,
+    DescriptionSubtitle,
+    MainButton,
+    MainWrapper,
+    NawazImg,
 } from "./styles";
 
 const Main = () => (
-  <MainWrapper>
-    <NawazImg />
-    <DescriptionWrapper>
-      <div className="poppins">
-        <DescriptionTitle>Introducing</DescriptionTitle>
-        <DescriptionTitle>Dr. Mirweiss Nawaz</DescriptionTitle>
-        <DescriptionSubtitle>DDS, PLLC</DescriptionSubtitle>
-      </div>
-      <div className="nunito">
-        <Description>
-          Since its opening in 2007, Dr. Nawaz has been devoted to delivering
-          excellent dental care at Family & Cosmetic Dentistry. Proficient in
-          family and cosmetic dentistry, he creates tailored treatment plans for
-          each patient, ensuring the highest quality care in a welcoming
-          environment through his dedication to staying current with dental
-          advancements.
-        </Description>
-        <DescriptionLists>
-          <ul style={{ listStyleType: "circle" }}>
-            <li style={{ color: "#00ACB1", marginBottom: "1.5rem" }}>
+    <MainWrapper>
+        <NawazImg />
+        <DescriptionWrapper>
+            <div className="poppins">
+                <DescriptionTitle>Introducing</DescriptionTitle>
+                <DescriptionTitle>Dr. Mirweiss Nawaz</DescriptionTitle>
+                <DescriptionSubtitle>DDS, PLLC</DescriptionSubtitle>
+            </div>
+            <div className="nunito">
+                <Description>
+                    Since its opening in 2007, Dr. Nawaz has been devoted to delivering
+                    excellent dental care at Family & Cosmetic Dentistry. Proficient in
+                    family and cosmetic dentistry, he creates tailored treatment plans for
+                    each patient, ensuring the highest quality care in a welcoming
+                    environment through his dedication to staying current with dental
+                    advancements.
+                </Description>
+                <DescriptionLists>
+                    <ul style={{ listStyleType: "circle" }}>
+                        <li style={{ color: "#00ACB1", marginBottom: "1.5rem" }}>
               <span style={{ color: "#000" }}>
                 Experienced dentist with over two decades of practice.
               </span>
-            </li>
-            <li style={{ color: "#00ACB1", marginBottom: "1.5rem" }}>
+                        </li>
+                        <li style={{ color: "#00ACB1", marginBottom: "1.5rem" }}>
               <span style={{ color: "#000" }}>
                 Trusted city dentist with over twenty 5
                 <StarSharp
-                  style={{ color: "#00acb1", margin: "-0.4rem 0.2rem 0 0" }}
+                    style={{ color: "#00acb1", margin: "-0.4rem 0.2rem 0 0" }}
                 />
                 reviews
               </span>
-            </li>
-            <li style={{ color: "#00ACB1" }}>
+                        </li>
+                        <li style={{ color: "#00ACB1" }}>
               <span style={{ color: "#000" }}>
                 Committed to the best experience with the most modern dental
                 technology
               </span>
-            </li>
-          </ul>
-        </DescriptionLists>
-      </div>
-      <MainButton>
-        <Link href="/about" className="poppins">
-          Learn More!
-        </Link>
-      </MainButton>
-    </DescriptionWrapper>
-  </MainWrapper>
+                        </li>
+                    </ul>
+                </DescriptionLists>
+            </div>
+            <Link href="/about" className="poppins">
+                <MainButton>
+                    Learn More!
+                </MainButton>
+            </Link>
+        </DescriptionWrapper>
+    </MainWrapper>
 );
 export default Main;
--- src/app/home/components/Testimonials/ViewAllReviewsButton/index.jsx
+++ src/app/home/components/Testimonials/ViewAllReviewsButton/index.jsx
@@ -4,15 +4,15 @@ import { ViewAllButtonWrapper, ViewAllReviewsWrapper } from "./styles";
 
 const ViewAllReviewsButton = () => (
     <ViewAllReviewsWrapper>
-        <ViewAllButtonWrapper>
-            <Link
-                className="nunito"
-                href="https://www.google.com/search?q=Nawaz+Mirweiss+DDS&sca_esv=556867888&sxsrf=AB5stBh17De87ZOl6TMJFMALXlgQfnyvNQ%3A1692042763456&ei=C4baZNeyG9Kh5NoPj8GbqAk&ved=0ahUKEwiX5PC-9tyAAxXSEFkFHY_gBpUQ4dUDCBA&uact=5&oq=Nawaz+Mirweiss+DDS&gs_lp=Egxnd3Mtd2l6LXNlcnAiEk5hd2F6IE1pcndlaXNzIEREUzICECZIxQhQnARYnARwA3gAkAEAmAGGAaABhgGqAQMwLjG4AQPIAQD4AQL4AQHCAgkQABgHGB4YsAPCAgkQABgIGB4YsAPiAwQYASBBiAYBkAYC&sclient=gws-wiz-serp#lrd=0x89b63994a7c0b58d:0xa93e21972b785510,1,,,,"
-                target="_blank"
-            >
+        <Link
+            className="nunito"
+            href="https://www.google.com/search?q=Nawaz+Mirweiss+DDS&sca_esv=556867888&sxsrf=AB5stBh17De87ZOl6TMJFMALXlgQfnyvNQ%3A1692042763456&ei=C4baZNeyG9Kh5NoPj8GbqAk&ved=0ahUKEwiX5PC-9tyAAxXSEFkFHY_gBpUQ4dUDCBA&uact=5&oq=Nawaz+Mirweiss+DDS&gs_lp=Egxnd3Mtd2l6LXNlcnAiEk5hd2F6IE1pcndlaXNzIEREUzICECZIxQhQnARYnARwA3gAkAEAmAGGAaABhgGqAQMwLjG4AQPIAQD4AQL4AQHCAgkQABgHGB4YsAPCAgkQABgIGB4YsAPiAwQYASBBiAYBkAYC&sclient=gws-wiz-serp#lrd=0x89b63994a7c0b58d:0xa93e21972b785510,1,,,,"
+            target="_blank"
+        >
+            <ViewAllButtonWrapper>
                 View All 20+ 5-Star Reviews!
-            </Link>
-        </ViewAllButtonWrapper>
+            </ViewAllButtonWrapper>
+        </Link>
     </ViewAllReviewsWrapper>
 )
 
